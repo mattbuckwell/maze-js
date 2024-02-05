ddcc3940842ef61404af31d8c03e77b14436e6f6
Matt B <mattbuckwell@gmail.com>
2024-02-05 19:09:44 +1300

    adding additional notes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 const engine = Engine.create(); // create a new engine
 // get access to the 'world' that was create when we created a new engine
 const { world } = engine;
-// render object to actually show some content on the screen
+// render object to actually show some content on the screen using a canvas element
 const render = Render.create({
   // tell the render where we want to show our representation of world inside our html document
   element: document.body,
@@ -25,9 +25,11 @@ Render.run(render);
 Runner.run(Runner.create(), engine);
 
 // bodies object represents the ability to create shapes
+// - first two numbers represent position in the world, x and y values (centre of the rectangle)
+// - second two numbers represent the size of the shape, width and height
 const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true, // options object
+  isStatic: true, // options object - isStatic means it will show but will never move
 });
 
-// world is a snapshot of all the shapes we have
+// world is a snapshot of all the shapes we have and makes it show up on our canvas
 World.add(world, shape);
