2df14c8ce8b695f49369a22cf87eee9c21ce7c85
Matt B <mattbuckwell@gmail.com>
2024-02-06 13:21:48 +1300

    drawing borders on the canvas

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ Render.run(render);
 // this is what coordinates the changes in state a to state b
 Runner.run(Runner.create(), engine);
 
+/*
 // bodies object represents the ability to create shapes
 // - first two numbers represent position in the world, x and y values (centre of the rectangle)
 // - second two numbers represent the size of the shape, width and height
@@ -33,3 +34,16 @@ const shape = Bodies.rectangle(200, 200, 50, 50, {
 
 // world is a snapshot of all the shapes we have and makes it show up on our canvas
 World.add(world, shape);
+*/
+
+// Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+];
+World.add(world, walls);
+
+// adding rectangle to world
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
