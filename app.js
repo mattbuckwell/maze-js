4d9fe22c2081e9c230a45bb664d0f9ec75064cc9
Matt B <mattbuckwell@gmail.com>
2024-04-16 18:17:08 +1200

    refactoring maze for rectangle display

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 // adstracting values out of the code to be more flexible
-const cells = 6;
+const cellsHorizontal = 10;
+const cellsVertical = 8;
 // gets the value of the viewable screen of the user
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const unitLength = width / cells;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const engine = Engine.create();
 engine.world.gravity.y = 0;
@@ -59,9 +61,9 @@ const shuffle = (arr) => {
   return arr;
 };
 
-const grid = Array(cells)
+const grid = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells).fill(false));
+  .map(() => Array(cellsHorizontal).fill(false));
 /*
     Array(3) - create an empty array that has 3 possible places in it (has no elements)
 
@@ -74,19 +76,19 @@ const grid = Array(cells)
     -- this would look the same but all 3 arrays point to the same one, change one change them all
   */
 
-const verticals = Array(cells)
+const verticals = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells - 1).fill(false));
+  .map(() => Array(cellsHorizontal - 1).fill(false));
 
-const horizontals = Array(cells - 1)
+const horizontals = Array(cellsVertical - 1)
   .fill(null)
-  .map(() => Array(cells).fill(false));
+  .map(() => Array(cellsHorizontal).fill(false));
 
 // console.log(horizontals);
 
 // Pick a random starting point
-const startRow = Math.floor(Math.random() * cells);
-const startColumn = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
+const startColumn = Math.floor(Math.random() * cellsHorizontal);
 
 // define a function to call over and over using the startRow and startColumn and
 // inside the function, we are going to go through the algorithm for checking if a cell
@@ -115,9 +117,9 @@ const stepThroughCell = (row, column) => {
     // -- See if that neighbour is out of bounds
     if (
       nextRow < 0 ||
-      nextRow >= cells ||
+      nextRow >= cellsVertical ||
       nextColumn < 0 ||
-      nextColumn >= cells
+      nextColumn >= cellsHorizontal
     ) {
       continue;
     }
@@ -153,9 +155,9 @@ horizontals.forEach((row, rowIndex) => {
     }
     // formula for the x and y coordinates
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength / 2,
-      rowIndex * unitLength + unitLength,
-      unitLength,
+      columnIndex * unitLengthX + unitLengthX / 2,
+      rowIndex * unitLengthY + unitLengthY,
+      unitLengthX,
       10,
       {
         isStatic: true,
@@ -174,10 +176,10 @@ verticals.forEach((row, rowIndex) => {
     }
 
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength,
-      rowIndex * unitLength + unitLength / 2,
+      columnIndex * unitLengthX + unitLengthX,
+      rowIndex * unitLengthY + unitLengthY / 2,
       10,
-      unitLength,
+      unitLengthY,
       {
         isStatic: true,
         label: "wall",
@@ -190,10 +192,10 @@ verticals.forEach((row, rowIndex) => {
 // --- GOAL ---
 // code for the goal to be displayed in the maze and the bottom ride corner
 const goal = Bodies.rectangle(
-  width - unitLength / 2,
-  height - unitLength / 2,
-  unitLength * 0.7,
-  unitLength * 0.7,
+  width - unitLengthX / 2,
+  height - unitLengthY / 2,
+  unitLengthX * 0.7,
+  unitLengthY * 0.7,
   {
     isStatic: true,
     label: "goal",
@@ -202,8 +204,10 @@ const goal = Bodies.rectangle(
 World.add(world, goal);
 
 // --- BALL ---
+
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 // first two arguments to a circle is x and y coordinates of the circle
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
   label: "ball",
 });
 World.add(world, ball);
